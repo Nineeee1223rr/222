ff6964236f9331cb38f6cab2944a2c1b6c913b9c
Nineeee1223rr <103894855+Nineeee1223rr@users.noreply.github.com>
2024-06-12 10:08:26 +0800

    h

---
+++ i18n.config.ts
@@ -0,0 +1,23 @@
+export default defineI18nConfig(() => ({
+    legacy: false,
+    messages: {
+        en: {
+            setting: 'Setting',
+            use_own_key: 'Use own key',
+            new_chat: 'New chat',
+            input_password: 'Please enter access password',
+            confirm: 'Confirm',
+            with_history: 'Send with history',
+            without_history: 'Send without history',
+            please_input_text: 'Please input text',
+            add_image: 'Add image',
+            support_paste: 'Support paste',
+            send: 'Send',
+            img_gen_steps: 'Image generation steps',
+            text_generation: 'Text generation',
+            image_generation: 'Image generation',
+            system_prompt: 'System prompt',
+            universal: 'Universal',
+        }
+    }
+}))
